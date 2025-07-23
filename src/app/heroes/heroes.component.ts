bc1babe11cdfa8b6842c953c1b0e2224adcf3e5d
Hannah Campbell <82469258+palindromesoup@users.noreply.github.com>
2025-07-23 00:41:15 -0400

    Add hero.ts, update heroes component template to include form

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Hero } from '../hero';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.css'
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
